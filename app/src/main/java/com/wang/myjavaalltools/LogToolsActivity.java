61e8c7543a8ad0475eca631019917bed3cee9992
ohuo133233 <1441289824@qq.com>
2022-11-18 15:03:06 +0800

    退到gradle-6.7.1并增加阿里云私有仓库

--- app/src/main/java/com/wang/myjavaalltools/LogToolsActivity.java
+++ app/src/main/java/com/wang/myjavaalltools/LogToolsActivity.java
@@ -1,13 +1,13 @@
 package com.wang.myjavaalltools;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.wang.logtools.KLog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 
 public class LogToolsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,7 +18,7 @@ public class LogToolsActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_tools);
-        KLog.d("测试");
+//        KLog.d("测试");
 
         initView();
     }
@@ -36,23 +36,23 @@ public class LogToolsActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.start_line_number:
-                KLog.init(true);
-                KLog.e("测试");
-                break;
-            case R.id.close_line_number:
-                KLog.init(false);
-                KLog.e("测试");
-                break;
-            case R.id.long_log:
-                String ten = "0123456789";
-                for (int i = 1; i <= 10; i++) {
-                    Log.d(TAG, "i" + i);
-                    ten += ten;
-                }
-                KLog.e(TAG, ten+"-end");
-                break;
-        }
+//        switch (v.getId()) {
+//            case R.id.start_line_number:
+//                KLog.init(true);
+//                KLog.e("测试");
+//                break;
+//            case R.id.close_line_number:
+//                KLog.init(false);
+//                KLog.e("测试");
+//                break;
+//            case R.id.long_log:
+//                String ten = "0123456789";
+//                for (int i = 1; i <= 10; i++) {
+//                    Log.d(TAG, "i" + i);
+//                    ten += ten;
+//                }
+//                KLog.e(TAG, ten + "-end");
+//                break;
+//        }
     }
 }
