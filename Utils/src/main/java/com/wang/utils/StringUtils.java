ce668b4ebedfd42a99c3fac1112c4d244aae88bf
ohuo133233 <41932817+ohuo133233@users.noreply.github.com>
2022-12-07 20:44:10 +0800

    StringUtils 完善
    1.增加删除所有空格方法
    2.判断是否是HTTP格式方法

--- Utils/src/main/java/com/wang/utils/StringUtils.java
+++ Utils/src/main/java/com/wang/utils/StringUtils.java
@@ -16,7 +16,48 @@ public class StringUtils {
     }
 
 
+    /**
+     * 去掉所有空格
+     *
+     * @param string 需要去掉空格的字符串
+     * @return 去掉空格后的字符串
+     */
+    public static String removeAllSpace(String string) {
+        return string.replaceAll(" +", "");
+    }
 
+    /**
+     * 判断是否是HTTP格式
+     *
+     * @param string 需要判断的字符串
+     * @return 返回是否是HTTP格式
+     */
+    public static boolean isHttp(String string) {
+        // 判断是否是空的
+        if (isEmpty(string)) {
+            return false;
+        }
 
+        // 去首尾空格
+        string = string.trim();
+
+        // 把字符串转化为小写，后续判断就可以忽略大小写问题
+        string = string.toLowerCase();
+
+        // 判断是否包含http
+        if (!string.contains("http")) {
+            return false;
+        }
+
+        // 按http的分割
+        String[] https = string.split("http");
+
+        // 必须是第一个是http才是http
+        if (https[0].equals("http")) {
+            return true;
+        }
+
+        return false;
+    }
 
 }
