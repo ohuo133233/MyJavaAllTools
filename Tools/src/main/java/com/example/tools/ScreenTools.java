fcfc9bb06a3ab3b373c9c7550c392aecce96a4f3
ohuo133233 <1441289824@qq.com>
2023-05-31 20:42:32 +0800

    Update ScreenTools.java

--- Tools/src/main/java/com/example/tools/ScreenTools.java
+++ Tools/src/main/java/com/example/tools/ScreenTools.java
@@ -1,7 +1,11 @@
 package com.example.tools;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Point;
+import android.os.Build;
+import android.view.WindowManager;
 
 public class ScreenTools {
     private static final String TAG = "ScreenTools";
@@ -42,5 +46,35 @@ public class ScreenTools {
             e.printStackTrace();
         }
         return isLand;
+    }
+
+
+    public static int getScreenWidth(Application application) {
+        WindowManager wm = (WindowManager) application.getSystemService(Context.WINDOW_SERVICE);
+        if (wm == null) return -1;
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealSize(point);
+        } else {
+            wm.getDefaultDisplay().getSize(point);
+        }
+        return point.x;
+    }
+
+    /**
+     * Return the height of screen, in pixel.
+     *
+     * @return the height of screen, in pixel
+     */
+    public static int getScreenHeight(Application application) {
+        WindowManager wm = (WindowManager) application.getSystemService(Context.WINDOW_SERVICE);
+        if (wm == null) return -1;
+        Point point = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            wm.getDefaultDisplay().getRealSize(point);
+        } else {
+            wm.getDefaultDisplay().getSize(point);
+        }
+        return point.y;
     }
 }
