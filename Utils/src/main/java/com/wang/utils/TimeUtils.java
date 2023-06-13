b164dbe11fafbdc1f26f325c962c6be40d95dee5
ohuo133233 <1441289824@qq.com>
2023-06-13 21:22:25 +0800

    Update TimeUtils.java

--- Utils/src/main/java/com/wang/utils/TimeUtils.java
+++ Utils/src/main/java/com/wang/utils/TimeUtils.java
@@ -14,5 +14,24 @@ public class TimeUtils {
         return sdf.format(System.currentTimeMillis());
     }
 
+    /**
+     * 把毫秒转为小时、分钟、秒
+     * 列如:01:59:20
+     *
+     * @param milliseconds 传入毫秒
+     * @return 转换的字符串
+     */
+    public String convertMilliseconds(long milliseconds) {
+        // 计算小时、分钟和秒
+        long hours = milliseconds / (60 * 60 * 1000);
+        milliseconds %= (60 * 60 * 1000);
+        long minutes = milliseconds / (60 * 1000);
+        milliseconds %= (60 * 1000);
+        long seconds = milliseconds / 1000;
+
+        // 构建格式化的时间字符串
+        String time = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        return time;
+    }
 
 }
