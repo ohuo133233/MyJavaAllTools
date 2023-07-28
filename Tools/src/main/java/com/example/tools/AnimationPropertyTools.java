a51e2665792fc806e523b75fb3c3defe18b1bf74
ohuo133233 <1441289824@qq.com>
2023-07-28 19:24:34 +0800

    Update AnimationPropertyTools.java

--- Tools/src/main/java/com/example/tools/AnimationPropertyTools.java
+++ Tools/src/main/java/com/example/tools/AnimationPropertyTools.java
@@ -16,7 +16,7 @@ public class AnimationPropertyTools {
      * @param duration 持续时间
      * @param valuesX  放大X的倍率
      * @param valuesY  放大Y的倍率
-     * @param duration  放大Y的倍率
+     * @param duration 放大Y的倍率
      */
     public void scale(View view, float valuesX, float valuesY, long duration) {
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", valuesX);
@@ -35,12 +35,8 @@ public class AnimationPropertyTools {
      * @param translation_Y 平移到Y的坐标
      */
     public void translation(View view, long duration, float translation_x, float translation_Y) {
-        ObjectAnimator translationX = ObjectAnimator.ofFloat(view, "translationX", translation_x);
-        ObjectAnimator translationY = ObjectAnimator.ofFloat(view, "translationY", translation_Y);
-        translationX.setDuration(duration);
-        translationY.setDuration(duration);
-        translationX.start();
-        translationY.start();
+        translationX(view, duration, translation_x);
+        translationY(view, duration, translation_Y);
     }
 
     /**
@@ -50,10 +46,11 @@ public class AnimationPropertyTools {
      * @param duration   持续时间
      * @param alphaValue 透明度的值
      */
-    private void alpha(View view, long duration, float... alphaValue) {
+    private ObjectAnimator alpha(View view, long duration, float... alphaValue) {
         ObjectAnimator alpha = ObjectAnimator.ofFloat(view, "alpha", alphaValue);
         alpha.setDuration(duration);
         alpha.start();
+        return alpha;
     }
 
     /**
@@ -63,23 +60,26 @@ public class AnimationPropertyTools {
      * @param duration      持续时间
      * @param rotationValue 旋转的值
      */
-    private void rotation(View view, long duration, float... rotationValue) {
+    private ObjectAnimator rotation(View view, long duration, float... rotationValue) {
         ObjectAnimator rotation = ObjectAnimator.ofFloat(view, "rotation", rotationValue);
         rotation.setDuration(duration);
         rotation.start();
+        return rotation;
     }
 
 
-    public void translationX(View view, long duration, float... translationX) {
+    public ObjectAnimator translationX(View view, long duration, float... translationX) {
         ObjectAnimator translation = ObjectAnimator.ofFloat(view, "translationX", translationX);
         translation.setDuration(duration);
         translation.start();
+        return translation;
     }
 
-    public void translationY(View view, long duration, float... translationY) {
+    public ObjectAnimator translationY(View view, long duration, float... translationY) {
         ObjectAnimator translation = ObjectAnimator.ofFloat(view, "translationY", translationY);
         translation.setDuration(duration);
         translation.start();
+        return translation;
     }
 
 
